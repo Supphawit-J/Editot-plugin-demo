0030c947a2216f400315e3cc62f10be5930205ab
Supphawit-J <66815835+Supphawit-J@users.noreply.github.com>
2021-06-04 06:05:44 +0200

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DemoEditorComponent} from "./editor-plugin/demo-editor/demo-editor.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DemoEditorComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
