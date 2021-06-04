2bebae7e1afe7e747266a3dc1a1ee871b036ec94
Supphawit-J <66815835+Supphawit-J@users.noreply.github.com>
2021-06-04 06:06:46 +0200

    add new module and component

--- src/app/editor-plugin/editor-plugin.module.ts
+++ src/app/editor-plugin/editor-plugin.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DemoEditorComponent } from './demo-editor/demo-editor.component';
+import {CKEditorModule} from "@ckeditor/ckeditor5-angular";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DemoEditorComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CKEditorModule
   ]
 })
 export class EditorPluginModule { }
